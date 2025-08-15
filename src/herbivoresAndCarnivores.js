b236dc8365678d7962ff34798c87891d55dbea78
kirky359 <lol783176@gmail.com>
2025-08-15 22:00:16 +0300

    fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -10,13 +10,7 @@ class Animal {
   }
 
   checkIfAlive() {
-    if (this.health <= 0) {
-      const index = Animal.alive.indexOf(this);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
-      }
-    }
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
   }
 }
 
